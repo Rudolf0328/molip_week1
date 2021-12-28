dbdb20f9f2594e3af99eab43786c495ec244a689
이지원 <2019000982@hanyang.ac.kr>
2021-12-28 17:57:55 +0900

    fragment start

--- app/src/main/java/com/example/molip/MainActivity.java
+++ app/src/main/java/com/example/molip/MainActivity.java
@@ -1,14 +1,58 @@
 package com.example.molip;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    FragmentManager manager;
+    FragmentTransaction ft;
+
+    Phone phone;
+    Pictures pictures;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        manager = getSupportFragmentManager();
+
+        Button phoneTab = findViewById(R.id.one);
+        Button picturesTab = findViewById(R.id.two);
+
+        phone = new Phone();
+        pictures = new Pictures();
+
+        ft = manager.beginTransaction();
+        ft.add(R.id.fragment_container, phone);
+        ft.addToBackStack(null);
+        ft.commit();
+
+        phoneTab.setOnClickListener(this);
+        picturesTab.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        ft = manager.beginTransaction();
+
+        int id = v.getId();
+        switch (id) {
+            case R.id.one:
+                ft.replace(R.id.fragment_container, phone);
+                ft.commit();
+                break;
+            case R.id.two:
+                ft.replace(R.id.fragment_container, pictures);
+                ft.commit();
+                break;
+            default:
+                break;
+        }
     }
 }
