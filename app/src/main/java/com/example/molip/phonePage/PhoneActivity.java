9974e00b4bba5810e4293b7c0bad55336aa2a82d
이지원 <2019000982@hanyang.ac.kr>
2021-12-29 20:52:32 +0900

    기능은 다 완성

--- app/src/main/java/com/example/molip/phonePage/PhoneActivity.java
+++ app/src/main/java/com/example/molip/phonePage/PhoneActivity.java
@@ -2,15 +2,22 @@ package com.example.molip.phonePage;
 
 import static android.app.Activity.RESULT_OK;
 
+import static androidx.core.app.ActivityCompat.requestPermissions;
+import static androidx.core.content.PermissionChecker.checkCallingOrSelfPermission;
+
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -35,6 +42,10 @@ public class PhoneActivity extends Fragment {
     PhoneRcvAdapter rcvAdapter;
 //    private ArrayList<PhoneData> phoneData;
 
+//    String[] permission_list = {
+//            Manifest.permission.WRITE_CONTACTS
+//    };
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -45,6 +56,7 @@ public class PhoneActivity extends Fragment {
         rcvPhones.setLayoutManager(new LinearLayoutManager(getActivity()));
         rcvAdapter = new PhoneRcvAdapter(DummyData.dummyList, getActivity());
         rcvPhones.setAdapter(rcvAdapter);
+//        checkPermission();
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -52,6 +64,7 @@ public class PhoneActivity extends Fragment {
                 add.setData(ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
                 startActivityForResult(add, 0);
             }
+
         });
         return v;
     }
@@ -60,15 +73,14 @@ public class PhoneActivity extends Fragment {
         String sName = "error";
         String sNumber = "error";
         String sImage = "error";
-        String sPerson = "error";
+//        String sPerson = "error";
 
         if (resultCode == RESULT_OK) {
-            Cursor cursor = getActivity().getContentResolver().query(data.getData(), new String[]{ContactsContract.Contacts._ID, ContactsContract.Contacts.PHOTO_ID, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER}, null, null, null);
+            Cursor cursor = getActivity().getContentResolver().query(data.getData(), new String[]{ContactsContract.CommonDataKinds.Phone.PHOTO_URI, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER}, null, null, null);
             cursor.moveToFirst();
-            sPerson = cursor.getString(0);
-            sImage = cursor.getString(1);
-            sName = cursor.getString(2);
-            sNumber = cursor.getString(3);
+            sImage = cursor.getString(0);
+            sName = cursor.getString(1);
+            sNumber = cursor.getString(2);
             cursor.close();
         }
         System.out.println("image: " + sImage);
@@ -77,4 +89,38 @@ public class PhoneActivity extends Fragment {
         rcvPhones.setAdapter(rcvAdapter);
         super.onActivityResult(requestCode, resultCode, data);
     }
+
+//    public void checkPermission(){
+//        //현재 안드로이드 버전이 6.0미만이면 메서드를 종료한다.
+//        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M)
+//            return;
+//
+//        for(String permission : permission_list){
+//            //권한 허용 여부를 확인한다.
+//            int chk = checkCallingOrSelfPermission(getContext(), permission);
+//
+//            if(chk == PackageManager.PERMISSION_DENIED){
+//                //권한 허용을여부를 확인하는 창을 띄운다
+//                requestPermissions(permission_list,0);
+//            }
+//        }
+//    }
+//
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        if(requestCode==0)
+//        {
+//            for(int i=0; i<grantResults.length; i++)
+//            {
+//                //허용됬다면
+//                if(grantResults[i]==PackageManager.PERMISSION_GRANTED){
+//                }
+//                else {
+////                    Toast.makeText(getApplicationContext(),"앱권한설정하세요",Toast.LENGTH_LONG).show();
+//////                    finish();
+//                }
+//            }
+//        }
+//    }
 }
