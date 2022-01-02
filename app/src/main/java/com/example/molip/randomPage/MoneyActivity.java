bb5224741284266a5cf65a333041d9a2182a57be
이지원 <2019000982@hanyang.ac.kr>
2022-01-02 19:24:52 +0900

    random money

--- app/src/main/java/com/example/molip/randomPage/MoneyActivity.java
+++ app/src/main/java/com/example/molip/randomPage/MoneyActivity.java
@@ -1,6 +1,10 @@
 package com.example.molip.randomPage;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.Selection;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -8,11 +12,13 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.molip.R;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -43,7 +49,6 @@ public class MoneyActivity extends AppCompatActivity {
         btnDelete = (Button) findViewById(R.id.money_btn_delete);
         btnGo = (Button) findViewById(R.id.money_btn_go);
         llName = (LinearLayout) findViewById(R.id.money_ll_name);
-
 
         btnGood.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -82,12 +87,16 @@ public class MoneyActivity extends AppCompatActivity {
                 EditText et = new EditText(getApplicationContext());
                 TextView tv = new TextView(getApplicationContext());
                 LinearLayout ll = new LinearLayout(getApplicationContext());
-                LinearLayout.LayoutParams p1 = new LinearLayout.LayoutParams(700, ViewGroup.LayoutParams.WRAP_CONTENT);
+                LinearLayout.LayoutParams p1 = new LinearLayout.LayoutParams(650, ViewGroup.LayoutParams.WRAP_CONTENT);
                 LinearLayout.LayoutParams p2 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
                 et.setLayoutParams(p1);
                 et.setHint("이름을 입력하세요.");
-                p2.setMargins(100, 0, 0, 0);
+                p2.setMargins(30, 0, 0, 0);
                 tv.setLayoutParams(p2);
+//                tv.setTextColor(Integer.parseInt("#FFFFFF"));
+                tv.setTextColor(Color.parseColor("#000000"));
+                tv.setTextSize(18);
+                tv.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_END);
 //                tv.setText("0");
                 tvs.add(tv);
                 ll.setOrientation(LinearLayout.HORIZONTAL);
@@ -120,107 +129,121 @@ public class MoneyActivity extends AppCompatActivity {
             public void onClick(View view) {
                 ArrayList<Integer> prices = new ArrayList<>(num);
                 ArrayList<Double> ratios = new ArrayList<>(num);
-                total = Integer.parseInt(etTotal.getText().toString());
-                avgInt = total / num;
+                String totalString = etTotal.getText().toString();
 
-                avgDouble = (double) total / (double) num;
+                totalString = totalString.trim();
 
-                double ratio = 1.0 / num;
-                int test = 0;
-                System.out.println("ratio : " + ratio);
-                switch (level) {
-                    case 0:
-                        low = avgInt - (avgInt * 0.1);
-                        high = avgInt + (avgInt * 0.1);
+                if (totalString.getBytes().length <= 0) {
+                    Toast.makeText(MoneyActivity.this, "총 금액 입력하세요.", Toast.LENGTH_SHORT).show();
+                } else if (totalString.getBytes().length >= 10) {
+                    Toast.makeText(MoneyActivity.this, "10억 미만의 금액 입력하세요.", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (num == 0) {
+                        Toast.makeText(MoneyActivity.this, "한 명 이상 추가해주세요.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        total = Integer.parseInt(totalString);
+                        avgInt = total / num;
 
-                        for (int i = 0; i < num; i++) {
-                            double tmp = Math.random() * (high - low) + low;
+                        avgDouble = (double) total / (double) num;
+
+                        double ratio = 1.0 / num;
+                        int test = 0;
+                        System.out.println("ratio : " + ratio);
+                        switch (level) {
+                            case 0:
+                                low = avgInt - (avgInt * 0.1);
+                                high = avgInt + (avgInt * 0.1);
+
+                                for (int i = 0; i < num; i++) {
+                                    double tmp = Math.random() * (high - low) + low;
 //                            ratios.add(tmp);
-                            prices.add((int) (tmp));
-                        }
+                                    prices.add((int) (tmp));
+                                }
 
-                        for (int i = 0; i < num; i++) {
-                            test += prices.get(i);
-                        }
+                                for (int i = 0; i < num; i++) {
+                                    test += prices.get(i);
+                                }
 
-                        if (test > total) {
-                            int diff = test - total;
-                            System.out.println("1. " + diff);
-                            int max = Collections.max(prices);
-                            int index = prices.indexOf(max);
-                            prices.remove(index);
-                            prices.add(index, max - diff);
-                        } else if (test < total) {
-                            int diff = total - test;
-                            System.out.println("2. " + diff);
-                            int min = Collections.min(prices);
-                            int index = prices.indexOf(min);
-                            prices.remove(index);
-                            prices.add(index, min + diff);
-                        }
+                                if (test > total) {
+                                    int diff = test - total;
+                                    System.out.println("1. " + diff);
+                                    int max = Collections.max(prices);
+                                    int index = prices.indexOf(max);
+                                    prices.remove(index);
+                                    prices.add(index, max - diff);
+                                } else if (test < total) {
+                                    int diff = total - test;
+                                    System.out.println("2. " + diff);
+                                    int min = Collections.min(prices);
+                                    int index = prices.indexOf(min);
+                                    prices.remove(index);
+                                    prices.add(index, min + diff);
+                                }
 
-                        break;
-                    case 1:
-                        low = avgInt - (avgInt * 0.5);
-                        high = avgInt + (avgInt * 0.5);
+                                break;
+                            case 1:
+                                low = avgInt - (avgInt * 0.5);
+                                high = avgInt + (avgInt * 0.5);
 
-                        for (int i = 0; i < num; i++) {
-                            double tmp = Math.random() * (high - low) + low;
+                                for (int i = 0; i < num; i++) {
+                                    double tmp = Math.random() * (high - low) + low;
 //                            ratios.add(tmp);
-                            prices.add((int) (tmp));
+                                    prices.add((int) (tmp));
+                                }
+
+                                test = 0;
+
+                                for (int i = 0; i < num; i++) {
+                                    test += prices.get(i);
+                                }
+
+                                if (test > total) {
+                                    int diff = test - total;
+                                    System.out.println("1. " + diff);
+                                    int min = Collections.min(prices);
+                                    int index = prices.indexOf(min);
+                                    prices.remove(index);
+                                    prices.add(index, min - diff);
+                                } else if (test < total) {
+                                    int diff = total - test;
+                                    System.out.println("2. " + diff);
+                                    int max = Collections.max(prices);
+                                    int index = prices.indexOf(max);
+                                    prices.remove(index);
+                                    prices.add(index, max + diff);
+                                }
+                                break;
+                            case 2:
+                                int tmp = (int) (Math.random() * num);
+                                System.out.println(tmp);
+
+                                for (int i = 0; i < num - 1; i++) {
+                                    prices.add(0);
+                                }
+                                prices.add(tmp, total);
+                                break;
+                            default:
+                                break;
                         }
 
-                        test = 0;
+                        int testtest = 0;
 
                         for (int i = 0; i < num; i++) {
-                            test += prices.get(i);
+                            testtest += prices.get(i);
                         }
 
-                        if (test > total) {
-                            int diff = test - total;
-                            System.out.println("1. " + diff);
-                            int min = Collections.min(prices);
-                            int index = prices.indexOf(min);
-                            prices.remove(index);
-                            prices.add(index, min - diff);
-                        } else if (test < total) {
-                            int diff = total - test;
-                            System.out.println("2. " + diff);
-                            int max = Collections.max(prices);
-                            int index = prices.indexOf(max);
-                            prices.remove(index);
-                            prices.add(index, max + diff);
+                        System.out.println("adjust : " + testtest);
+
+                        System.out.println(ratios);
+                        System.out.println(prices);
+                        String avgText = "Average : " + avgInt + "원";
+                        tvAvg.setText(avgText);
+
+                        for (int i = 0; i < num; i++) {
+                            String priceText = prices.get(i) + "원";
+                            tvs.get(i).setText(priceText);
                         }
-                        break;
-                    case 2:
-                        int tmp = (int) (Math.random() * num);
-                        System.out.println(tmp);
-
-                        for (int i = 0; i < num-1; i++) {
-                            prices.add(0);
-                        }
-                        prices.add(tmp, total);
-                        break;
-                    default:
-                        break;
-                }
-
-                int testtest = 0;
-
-                for (int i = 0; i < num; i++) {
-                    testtest += prices.get(i);
-                }
-
-                System.out.println("adjust : " + testtest);
-
-                System.out.println(ratios);
-                System.out.println(prices);
-                String avgText = "Average : " + avgDouble + "원";
-                tvAvg.setText(avgText);
-
-                for (int i = 0; i < num; i++) {
-                    String priceText = prices.get(i) + "원";
-                    tvs.get(i).setText(priceText);
+                    }
                 }
             }
         });
